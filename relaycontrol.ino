c9e891adfb471c80034566f1407510e81f9d1321
khutjo <kmaputla@student.wethinkcode.co.za>
2022-04-30 10:46:38 +0200

    simple but elegent

---
+++ relaycontrol.ino
@@ -0,0 +1,111 @@
+// Adafruit IO Subscription Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+
+// set up the 'counter' feed
+AdafruitIO_Feed *counter = io.feed(FEED_MANE);
+
+int contact[] = {PIN_A, PIN_B};
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(contact[0], OUTPUT);
+  pinMode(contact[1], OUTPUT);
+  digitalWrite(contact[0], HIGH);
+  digitalWrite(contact[1], HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // start MQTT connection to io.adafruit.com
+  io.connect();
+
+  // set up a message handler for the count feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  counter->onMessage(handleMessage);
+
+  // wait for an MQTT connection
+  // NOTE: when blending the HTTP and MQTT API, always use the mqttStatus
+  // method to check on MQTT connection status specifically
+  while(io.mqttStatus() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // Because Adafruit IO doesn't support the MQTT retain flag, we can use the
+  // get() function to ask IO to resend the last value for this feed to just
+  // this MQTT client after the io client is connected.
+  counter->get();
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  // Because this sketch isn't publishing, we don't need
+  // a delay() in the main program loop.
+
+}
+
+// this function is called whenever a 'counter' message
+// is received from Adafruit IO. it was attached to
+// the counter feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *data) {
+
+  Serial.print("received <- ");
+  Serial.println(data->value());
+  runswitch(data->toString());
+}
+
+void runswitch(String data){
+  if(data == ACTION_ID_A){
+    digitalWrite(contact[0], LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(contact[0], HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }else if(data == ACTION_ID_B){
+    digitalWrite(contact[1], LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(contact[1], HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+
+}
